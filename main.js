bc87775107207938ecd7b993c280f7a0bf4ef9be
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-20 18:16:11 +0000

    A metà del secondo snecchino

--- main.js
+++ main.js
@@ -2,12 +2,6 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
 
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
@@ -22,9 +16,6 @@ var z = str.length;                                                     // per a
 var array =Array.from(str);                                       // Carico l'array
 var inizio = "";
 var fine = "";
-let prova = array[2];
-            console.log(array);
-            console.log(prova);
 
 if (y % 2 != 0) {                                                           // Controllo se il numero è pari, altrimenti sottracco 1 e divido per 2
     y = ((y - 1) / 2)
@@ -33,16 +24,13 @@ else {
     y= y /2
 }
         console.log(y);
-        // document.getElementById("testo)").innerHTML = `La parola ${str}`;
+            document.getElementById("testo").innerHTML = `La parola ${str}`;
 
 for ( i = 0; i  <= y; i++) {
         z --;
         inizio = array[i];
         fine = array[z];
 
-        console.log ( inizio );
-        console.log ( fine );
-        
     if (inizio != fine){
 
         console.log(z)
@@ -51,7 +39,6 @@ for ( i = 0; i  <= y; i++) {
     }
 
     else {
- 
         document.getElementById("output").innerHTML = `La parola è palindroma`;
         break;}
 }
@@ -60,3 +47,22 @@ for ( i = 0; i  <= y; i++) {
 // tuttottut
 // cosasoc
 // !SECTION 
+
+// SECTION
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+let pariDispari = prompt("Scegli pari (p) o dispari (d)");                                                                        // Pari o dispari
+let numeroInserito =  prompt("Scegli un numero");                                                                               // Variabile da 1 a cinque
+let pc = randomPc()
+let somma = numeroInserito + pc
+console.log( pariDispari, numeroInserito, pc, somma)
+
+function randomPc() {
+    let x = Math.floor((Math.random () * 5));
+    return x;
+}
